fa7b32ed01e66ee7067e45f8dc0425093da579d9
Mohammad Rahman <mbrahman@gmail.com>
2022-03-12 20:02:24 -0500

    added post functionality

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,13 +1,36 @@
 const router = require('express').Router()
+const { v4: uuidv4 } = require('uuid')
+
 const { notes } = require('../../db/db.json')
-const { findById } = require('../../lib/notes')
+const { filterByQuery, findById, createNewNote, validateNote } = require('../../lib/notes')
 
 router.get('/notes', (req, res) => {
-    res.json(notes)
+    let results = notes
+    if (req.query) {
+        results = filterByQuery(req.query, notes)
+    }
+    res.json(results)
 })
 
 router.get('/notes/:id', (req, res) => {
-    res.json(findById(req.params.id, notes))
+    const result = findById(req.params.id, notes)
+    if (result) {
+        res.json(result)
+    } else {
+        res.send(404)
+    }
+})
+
+router.post('/notes', (req, res) => {
+    // adds a random uuid to incoming data
+    req.body.id = uuidv4()
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.')
+    } else {
+        const note = createNewNote(req.body, notes)
+        res.json(note)
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const notesRoutes = require('./routes/apiRoutes')
 const app = express()
 const PORT = process.env.PORT || 5001
 
+app.use(express.urlencoded({ extended: true }))
+// parse incoming JSON to JS Object
+app.use(express.json())
 app.use(express.static('public'))
 app.use('/', htmlRoutes)
 app.use('/api', notesRoutes)
